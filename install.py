8c3c329735695447fadd381b50c3dff79bc261da
Juliano Fernandes <julianofernandes@gmail.com>
2025-01-12 13:59:48 -0300

    Fail fast on unsupported platforms.

--- install.py
+++ install.py
@@ -8,3 +8,11 @@ if sys.version_info[:2] < MIN_VERSION:
     error = f"Unsupported Python version {current}, requires {required}"
     print(error, file=sys.stderr)
     exit(1)
+
+SUPPORTED_PLATFORMS = ("cygwin", "darwin", "linux")
+
+if sys.platform not in SUPPORTED_PLATFORMS:
+    platforms = ", ".join(SUPPORTED_PLATFORMS)
+    error = f"Unsupported platform {sys.platform}, must be one of: {platforms}"
+    print(error, file=sys.stderr)
+    exit(1)
