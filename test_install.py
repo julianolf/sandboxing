67b1a4225e7bce3d1904f86dffc808d83a15ce3a
Juliano Fernandes <julianofernandes@gmail.com>
2025-01-13 21:42:50 -0300

    Remove unnecessary fixtures from tests

--- test_install.py
+++ test_install.py
@@ -1,35 +1,28 @@
 import os
 
-import pytest
-
 import install
 
 
-@pytest.fixture(scope="session")
-def home():
-    return os.path.expanduser("~")
-
-
-def test_data_dir_on_darwin(home, monkeypatch):
-    expected = os.path.join(home, "Library", "Application Support")
+def test_data_dir_on_darwin(monkeypatch):
+    expected = os.path.join(install.HOME, "Library", "Application Support")
     monkeypatch.setattr(install.sys, "platform", "darwin")
     assert install.data_dir() == expected
 
 
-def test_data_dir_on_linux_with_xdg_data_home(home, monkeypatch):
-    expected = os.path.join(home, ".shared-data")
+def test_data_dir_on_linux_with_xdg_data_home(monkeypatch):
+    expected = os.path.join(install.HOME, ".shared-data")
     monkeypatch.setenv("XDG_DATA_HOME", expected)
     monkeypatch.setattr(install.sys, "platform", "linux")
     assert install.data_dir() == expected
 
 
-def test_data_dir_on_linux_without_xdg_data_home(home, monkeypatch):
-    expected = os.path.join(home, ".local", "share")
+def test_data_dir_on_linux_without_xdg_data_home(monkeypatch):
+    expected = os.path.join(install.HOME, ".local", "share")
     monkeypatch.delenv("XDG_DATA_HOME", raising=False)
     monkeypatch.setattr(install.sys, "platform", "linux")
     assert install.data_dir() == expected
 
 
-def test_bin_dir(home):
-    expected = os.path.join(home, ".local", "bin")
+def test_bin_dir():
+    expected = os.path.join(install.HOME, ".local", "bin")
     assert install.bin_dir() == expected
