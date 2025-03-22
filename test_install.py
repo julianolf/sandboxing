2bb52da7b9662e60bd8f0960166533e7715a2d53
Juliano Fernandes <julianofernandes@gmail.com>
2025-03-22 13:58:11 -0300

    Added tests for the link and unlink functions.

--- test_install.py
+++ test_install.py
@@ -1,4 +1,5 @@
 import argparse
+import contextlib
 import os
 import pathlib
 import tempfile
@@ -60,12 +61,22 @@ class TestPkgSrc(unittest.TestCase):
                 self.assertEqual(install.pkg_src(args), src)
 
 
+@contextlib.contextmanager
+def temp_dirs():
+    with tempfile.TemporaryDirectory() as path:
+        user_bin = pathlib.Path(path).resolve() / "bin"
+        user_bin.mkdir()
+
+        venv_dir = pathlib.Path(path).resolve() / "venv"
+        venv_bin = venv_dir / "bin"
+        venv_bin.mkdir(parents=True)
+
+        yield user_bin, venv_bin
+
+
 class TestPkgScripts(unittest.TestCase):
     def test_scripts_lookup(self):
-        with tempfile.TemporaryDirectory() as path:
-            venv_bin = pathlib.Path(path) / "bin"
-            venv_bin.mkdir()
-
+        with temp_dirs() as (_, venv_bin):
             files = (
                 "activate",
                 "activate.csh",
@@ -87,3 +98,41 @@ class TestPkgScripts(unittest.TestCase):
 
             expected = {"py.test", "pytest"}
             self.assertEqual(install.pkg_scripts(venv_bin), expected)
+
+
+class TestLink(unittest.TestCase):
+    def test_creates_link(self):
+        with temp_dirs() as (user_bin, venv_bin):
+            shouldnt_exist = venv_bin / "shouldnt_exist"
+            shouldnt_exist_link = user_bin / shouldnt_exist.name
+
+            should_exist = venv_bin / "should_exist"
+            should_exist.touch()
+            should_exist_link = user_bin / should_exist.name
+            should_exist_link.symlink_to(should_exist)
+
+            should_create = venv_bin / "should_create"
+            should_create.touch()
+            should_create_link = user_bin / should_create.name
+
+            bins = (shouldnt_exist.name, should_exist.name, should_create.name)
+            install.link(venv_bin, user_bin, bins)
+
+            self.assertFalse(shouldnt_exist_link.is_symlink())
+            self.assertTrue(should_exist_link.is_symlink())
+            self.assertTrue(should_create_link.is_symlink())
+
+
+class TestUnlink(unittest.TestCase):
+    def test_removes_link(self):
+        with temp_dirs() as (user_bin, venv_bin):
+            script = venv_bin / "script"
+            script.touch()
+
+            script_link = user_bin / script.name
+            script_link.symlink_to(script)
+
+            bins = (script.name,)
+            install.unlink(venv_bin, user_bin, bins)
+
+            self.assertFalse(script_link.is_symlink())
